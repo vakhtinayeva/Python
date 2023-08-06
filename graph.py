8959f4b5d0f9f28c8439fe2b66abe0d70e002ee9
eve-va <vakhtina.yeva@gmail.com>
2023-08-06 19:13:08 +0200

    Remove edges

--- graph.py
+++ graph.py
@@ -19,3 +19,43 @@ labels = dict(((i, j), i * rows + j ) for i, j in G.nodes())
 nx.draw_networkx(G, pos=pos, node_size=1000, labels=labels)
 plt.axis('off')
 plt.show()
+
+# setting the max number of nodes to delete
+max_deleted = rows*columns - rows - columns + 1
+
+# function to remove edges
+def delete_edges(num: int):
+  # quantity to delete
+  to_delete = num
+  # deleted counter
+  deleted = 0
+
+  # checking if the number's within limits
+  if to_delete > max_deleted:
+    print('Impossible. Max allowed number is: %d' %max_deleted)
+    return
+
+  while deleted < to_delete:
+    # converting edges into list
+    edges = list(G.edges)
+    # choosing random edge
+    cur_edge = random.choice(edges)
+    # removing it
+    G.remove_edge(cur_edge[0], cur_edge[1])
+    # checking if the graph's still connected
+    connected = nx.is_connected(G)
+    
+    # adding 1 to counter and continuing
+    if connected:
+      deleted += 1
+    # or returning the edge and trying again
+    else:
+      G.add_edge(cur_edge[0], cur_edge[1])
+
+# calling functrion
+delete_edges(10)
+
+# drawing the graph with removed edges
+nx.draw_networkx(G, pos=pos, node_size=1000, labels=labels)
+plt.axis('off')
+plt.show()
